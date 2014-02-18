2daf7809211df0d11e12c2343691efeddc8d989a
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2014-02-19 00:28:59 +0100

    pass test 6

--- src/bindnames.c
+++ src/bindnames.c
@@ -4,7 +4,14 @@ char* thisClass;
 
 int bind_default ( node_t *root, int stackOffset)
 {
-	
+    for (int i=0; i<root->n_children; i++) {
+        node_t *child = root->children[i];
+        if (child != NULL) {
+            child->bind_names(child, stackOffset);
+        }
+    }
+
+    return 0;
 }
 
 int bind_constant ( node_t *root, int stackOffset)
@@ -12,6 +19,8 @@ int bind_constant ( node_t *root, int stackOffset)
 
 	if(outputStage == 6)
 		fprintf ( stderr, "CONSTANT\n");
+
+    return 0;
 	
 }
 
